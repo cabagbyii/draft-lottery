1880b68b0765ed7902aba20c9fc01f757e2a6028
Calvin <cabagbyii@gmail.com>
2018-07-31 11:02:42 -0400

    added factorial & permutation functions. Not sure if they are useful yet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function factorial(num){
+  if (num<0){
+    return -1;
+  } else if (num === 0){
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
+
+function permutation(numOfObj, permSize) {
+  let pre_d = numOfObj - permSize
+  let q = factorial(numOfObj);
+  let d = factorial(pre_d);
+  return q/d;
+}
+
+console.log('lottery perm', permutation(14, 4));
+
 class App extends Component {
   render() {
     return (
